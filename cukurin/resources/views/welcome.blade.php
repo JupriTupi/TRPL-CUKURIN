0dbdae6abd11b7a40635e6398d4f9473883f9a56
JupriTupi <55232226+JupriTupi@users.noreply.github.com>
2020-11-18 15:58:53 +0700

    Update welcome.blade.php

--- cukurin/resources/views/welcome.blade.php
+++ cukurin/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Care Pet
+                    Cukurin
                 </div>
 
                 <div class="links">
