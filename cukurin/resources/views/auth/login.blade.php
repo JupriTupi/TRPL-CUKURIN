f7e482feb0e5e39af817eb816457d694710e0049
JupriTupi <luthfidians@gmail.com>
2020-11-22 22:42:42 +0700

    update

--- cukurin/resources/views/auth/login.blade.php
+++ cukurin/resources/views/auth/login.blade.php
@@ -18,18 +18,16 @@
 </head>
 
 <body>
-    <div id="app">
+<div id="app">
     <section class="section">
         <div class="container mt-5">
             <div class="row">
                 <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-            <div class="login-brand">
-                CUKURIN
-            </div>
-
+                <div class="login-brand">
+                    CUKURIN
+                </div>
             <div class="card card-primary">
                 <div class="card-header"><h4>Masuk</h4></div>
-
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
                         @csrf
@@ -92,9 +90,9 @@
                 <div class="mt-1 text-muted text-center mb-5">
                     Belum Punya Akun? <a href="{{ route('register') }}">Daftar</a>
                 </div>
+                </div>
             </div>
             </div>
-        </div>
         </div>
     </section>
 </div>
